7d85434bc10a7f16a0c38db826d223d7700e95b2
Kain Highwind <highwind.1977@gmail.com>
2015-05-01 12:34:31 +0200

    some fixes and updates
    Change-Id: I595150a5700d549353c3adbfe1880e9b6bcdee8a

--- cm.mk
+++ cm.mk
@@ -1,5 +1,10 @@
+$(call inherit-product, device/lge/w7/full_w7.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -12,4 +17,7 @@ PRODUCT_NAME := cm_w7
 PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := w7
 
-$(call inherit-product, device/lge/w7/full_w7.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     PRODUCT_DEVICE="w7" \
+     BUILD_FINGERPRINT="lge/w7n_global_com/w7:5.1/LRX22G.A1428650077/1428650077:user/release-keys" \
+     PRIVATE_BUILD_DESC="w7_global_com-user 5.1 LRX22G A1428650077 release-keys"
